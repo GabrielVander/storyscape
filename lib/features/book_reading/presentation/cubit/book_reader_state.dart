2c3657a2e8740144cb62884c5a91c58d8967639f
gabriel <gabsvander@hotmail.com>
2024-01-22 23:33:36 -0300

    #2 Implement BookReaderCubit

--- lib/features/book_reading/presentation/cubit/book_reader_state.dart
+++ lib/features/book_reading/presentation/cubit/book_reader_state.dart
@@ -15,10 +15,30 @@ class BookReaderLoading extends BookReaderState {
 }
 
 class BookReaderDownloading extends BookReaderState {
-  const BookReaderDownloading({required this.percentageDisplay});
+  const BookReaderDownloading({required this.percentageDisplay, required this.percentageValue});
 
   final String percentageDisplay;
+  final double percentageValue;
 
   @override
-  List<Object> get props => [percentageDisplay];
+  List<Object> get props => [percentageDisplay, percentageValue];
+}
+
+class BookReaderError extends BookReaderState {
+  const BookReaderError({required this.errorCode, required this.context});
+
+  final String errorCode;
+  final String context;
+
+  @override
+  List<Object> get props => [errorCode, context];
+}
+
+class BookReaderFinished extends BookReaderState {
+  const BookReaderFinished({required this.content});
+
+  final Uint8List content;
+
+  @override
+  List<Object> get props => [content];
 }
