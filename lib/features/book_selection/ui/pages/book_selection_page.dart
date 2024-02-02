4e1059298ac24e4d55a06cf94ab3e925d7175908
gabriel <gabsvander@hotmail.com>
2024-02-01 22:37:15 -0300

    #20 Add cubit usage to BookSelectionPage

--- lib/features/book_selection/ui/pages/book_selection_page.dart
+++ lib/features/book_selection/ui/pages/book_selection_page.dart
@@ -1,10 +1,16 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooked_bloc/hooked_bloc.dart';
 import 'package:storyscape/core/routing/routes.dart';
-import 'package:storyscape/features/book_selection/ui/pages/book_url_field.dart';
+import 'package:storyscape/features/book_selection/ui/cubit/book_selection_cubit.dart';
+import 'package:storyscape/features/book_selection/ui/widgets/book_url_field.dart';
 
 class BookSelectionPage extends StatelessWidget {
-  const BookSelectionPage({super.key});
+  const BookSelectionPage({required BookSelectionCubit bookSelectionCubit, super.key})
+      : _bookSelectionCubit = bookSelectionCubit;
+
+  final BookSelectionCubit _bookSelectionCubit;
 
   @override
   Widget build(BuildContext context) {
@@ -15,11 +21,34 @@ class BookSelectionPage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(15),
         child: Center(
-          child: BookUrlField(
-            onFinished: (String url) => BookReadingRoute(url: url).push<void>(context),
+          child: _Body(
+            bookSelectionCubit: _bookSelectionCubit,
           ),
         ),
       ),
+    );
+  }
+}
+
+class _Body extends HookWidget {
+  const _Body({required this.bookSelectionCubit});
+
+  final BookSelectionCubit bookSelectionCubit;
+
+  @override
+  Widget build(BuildContext context) {
+    useBlocListener(
+      bookSelectionCubit,
+      (bloc, current, context) {
+        if (current case BookSelectionSelected(:final url)) {
+          BookReadingRoute(url: url).push<void>(context);
+        }
+      },
+      listenWhen: (current) => current is BookSelectionSelected,
+    );
+
+    return BookUrlField(
+      onFinished: bookSelectionCubit.selectBookUrl,
     );
   }
 }
